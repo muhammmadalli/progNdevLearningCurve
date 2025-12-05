52255cc9d4636078c6649cb7b9fd87c2579c02a6
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2025-12-05 23:47:46 +0500

    .gitignore recommit creatingAObje..\obj

--- basicsOfOOPwithCsharp/creatingAObject-Class-inCsharp/obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ basicsOfOOPwithCsharp/creatingAObject-Class-inCsharp/obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240bab44f031ec2bf622540b53664a62d6dc9b76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a67a0c63b415b2401f0745b6cbcd36651a8087")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
