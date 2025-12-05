086170ac80c475eea8201e768066d2e735ac58ec
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2025-12-05 23:56:28 +0500

    Update EntryPoint.cs

--- basicsOfOOPwithCsharp/creatingAObject-Class-inCsharp/EntryPoint.cs
+++ basicsOfOOPwithCsharp/creatingAObject-Class-inCsharp/EntryPoint.cs
@@ -7,8 +7,8 @@ class EntryPoint
         Point point = new Point();
     
         point.X = 5;
-        System.Console.WriteLine("Point X coordinate: " + point.X);
+        System.Console.WriteLine("Only Point X coordinate: " + point.X);
         point.Y = 20;
-        System.Console.WriteLine("Point coordinates: X = " + point.X + ", Y = " + point.Y);
+        System.Console.WriteLine("ALL Point coordinates: X = " + point.X + ", Y = " + point.Y);
     }
 }
