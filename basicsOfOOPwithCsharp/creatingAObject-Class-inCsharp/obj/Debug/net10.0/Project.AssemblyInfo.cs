240bab44f031ec2bf622540b53664a62d6dc9b76
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2025-12-05 23:04:38 +0500

    binaries updated during new project compile action

--- basicsOfOOPwithCsharp/creatingAObject-Class-inCsharp/obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ basicsOfOOPwithCsharp/creatingAObject-Class-inCsharp/obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a655487b1f86d7a1fac62c5f2062c09868cc09ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6058f03c09347707b3e21d802b84f7c4862de68d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
